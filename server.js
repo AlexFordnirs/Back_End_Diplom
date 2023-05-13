5181d5b7ece4a4ac369fca39e76bd9058c226e3e
AlexFordnirs <alexfordnir@gmail.com>
2023-05-13 19:28:42 +0300

    first commit

---
+++ server.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const testRoutes = require('./routes/test-routes');
+const userRoutes = require('./routes/user-routes');
+const adminRoutes = require('./routes/admin-routes');
+require('dotenv').config()
+const app = express();
+app.use(express.json());
+
+mongoose
+    .connect(process.env.URL, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then((res) => console.log('Connected to MongoDB'))
+    .catch((err) => console.log(`DB connection error: ${err}`));
+
+app.listen(process.env.PORT, (err) => {
+    err ? console.log(err) : console.log(`listening port ${process.env.PORT}`);
+});
+
+app.use(testRoutes);
+app.use(userRoutes);
+app.use(adminRoutes);
